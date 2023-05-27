ed10e1bff5b110129666aaaf7b40c30783d8d34c
Tan Hao Nguyen <101847895+tanhaok@users.noreply.github.com>
2023-05-27 17:21:13 +0700

    feat: add sonar for product (#5)

--- product/src/test/java/com/e/store/product/ProductApplicationTests.java
+++ product/src/test/java/com/e/store/product/ProductApplicationTests.java
@@ -1,13 +1,16 @@
 package com.e.store.product;
 
+import org.junit.Assert;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class ProductApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void text_alert() {
+		String msg = "HelloWorld";
+		Assertions.assertEquals("HelloWorld", msg);
 	}
 
 }
