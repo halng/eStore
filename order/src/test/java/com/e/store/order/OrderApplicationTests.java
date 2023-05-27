e9ba7153db08fb72858600d81756f3b0903c6c35
tanhaok <haonguyentan2001@gmail.com>
2023-05-27 15:36:44 +0700

    init project and workflow

---
+++ order/src/test/java/com/e/store/order/OrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.e.store.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
