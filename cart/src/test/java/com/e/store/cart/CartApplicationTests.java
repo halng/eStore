5cdbb362624f94df7e2c386d43f032365d61ee1d
Tan Hao Nguyen <101847895+tanhaok@users.noreply.github.com>
2023-05-27 15:41:09 +0700

    feat: init backend service (#2)

---
+++ cart/src/test/java/com/e/store/cart/CartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.e.store.cart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
