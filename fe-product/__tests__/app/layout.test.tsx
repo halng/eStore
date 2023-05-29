33760568108ffbc90693d24076127a3bdd60c674
tanhaok <haonguyentan2001@gmail.com>
2023-05-29 18:06:15 +0700

    fix: add unittest step in workflow

---
+++ fe-product/__tests__/app/layout.test.tsx
@@ -0,0 +1,9 @@
+import '@testing-library/jest-dom';
+
+describe('Test welcome tab title', () => {
+  it('Test get title', () => {
+    const msg = 'Create Next App';
+
+    expect("Create Next App").toEqual(msg);
+  });
+});
