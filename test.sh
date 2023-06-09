918914846892988432a097478d6be009cac348db
tanhaok <haonguyentan2001@gmail.com>
2023-06-09 20:05:01 +0700

    remove all workflow

--- test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-if [[ !("$1" != *"fix:"*) || !("$1" != *"init:"*) || !("$1" == *"feat:"*) || !("$1" != *"enhance:"*) ]];
-then 
-    echo "Commit message have to include fix, feat, init, enhance!"
-    exit 1
-fi
