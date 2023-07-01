9f0773ca77858508ab356b1af1e507ba89b3f58f
tanhaok <haonguyentan2001@gmail.com>
2023-07-01 10:47:49 +0700

    add function for active account

--- ui/api-estore/src/api/auth/index.ts
+++ ui/api-estore/src/api/auth/index.ts
@@ -5,8 +5,8 @@ const BASE_HOST = 'http://localhost:9090/api/v1/auth';
 const AuthAPI = {
   login: (data: any) => axios.post(`${BASE_HOST}/login`, data),
   register: (data: any) => axios.post(`${BASE_HOST}/register`, data),
-  verifyAccount: (accountId: String, token: String) =>
-    axios.post(`${BASE_HOST}/verify/${accountId}/${token}`),
+  activeAccount: (token: String, email: String) =>
+    axios.post(`${BASE_HOST}/active-account/${token}/${email}`),
 };
 
 export default AuthAPI;
