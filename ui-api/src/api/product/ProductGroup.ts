f4fe49fed00760c2563c6ae7d2352bbe74759c86
tanhaok <haonguyentan2001@gmail.com>
2023-08-24 22:21:48 +0700

    feat: integrate product group with backend

--- ui-api/src/api/product/ProductGroup.ts
+++ ui-api/src/api/product/ProductGroup.ts
@@ -2,6 +2,11 @@ import api from '../helper/api'
 
 const ProductGroupAPI = {
     create: (name: string) => api.post(`/product/group?groupName=${name}`),
+    getAll: (page: number) => api.get(`/product/group?page=${page}`),
+    update: (newName: string, groupId: number) => api.put(`/product/group/${groupId}?newName=${newName}`),
+    disableGroup: (groupId: number) => api.patch(`/product/group/${groupId}/disabled`),
+    enabledGroup: (groupId: number) => api.patch(`/product/group/${groupId}/enabled`),
+    deleteGroup: (groupId: number) => api.delete(`/product/group/${groupId}`),
 }
 
 export default ProductGroupAPI
--- ui-product/src/components/ProductGroup/index.tsx
+++ ui-product/src/components/ProductGroup/index.tsx
@@ -7,102 +7,98 @@ import { ProductGroupAPI } from "api-estore-v2";
 
 const ProductGroup = () => {
   const [groups, setGroups] = useState<ProductGroupType[]>([]);
+  const [page, setPage] = useState<number>(1);
+  const [totalPage, setTotalPage] = useState<number>(1);
+  const [totalGroups, setTotalGroups] = useState<number>(0);
   const [newGroup, setNewGroup] = useState<string>("");
-  const firstRow = ["Id", "Name", "Create Date", "Update Date", "Actions"];
+  const firstRow = [
+    "Id",
+    "Name",
+    "Status",
+    "Create Date",
+    "Update Date",
+    "Actions",
+  ];
+
+  const getAllGroup = () => {
+    ProductGroupAPI.getAll(page)
+      .then((res) => {
+        setGroups(res.data.groups);
+        setTotalPage(res.data.totalPages);
+        setTotalGroups(res.data.totalGroup);
+      })
+      .catch((err) => {
+        toast.error("Can't get all group! Try again later.");
+      });
+  };
+
   useEffect(() => {
-    setGroups([
-      {
-        id: 1,
-        name: "group 1",
-        createdDate: "2023-01-01",
-        updateDate: "2023-01-13",
-      },
-      {
-        id: 2,
-        name: "group 2",
-        createdDate: "2023-01-01",
-        updateDate: "2023-01-13",
-      },
-      {
-        id: 3,
-        name: "group 3",
-        createdDate: "2023-01-01",
-        updateDate: "2023-01-13",
-      },
-      {
-        id: 4,
-        name: "group 4",
-        createdDate: "2023-01-01",
-        updateDate: "2023-01-13",
-      },
-      {
-        id: 5,
-        name: "group 5",
-        createdDate: "2023-01-01",
-        updateDate: "2023-01-13",
-      },
-      {
-        id: 6,
-        name: "group 6",
-        createdDate: "2023-01-01",
-        updateDate: "2023-01-13",
-      },
-      {
-        id: 7,
-        name: "group 7",
-        createdDate: "2023-01-01",
-        updateDate: "2023-01-13",
-      },
-      {
-        id: 8,
-        name: "group 8",
-        createdDate: "2023-01-01",
-        updateDate: "2023-01-13",
-      },
-      {
-        id: 9,
-        name: "group 9",
-        createdDate: "2023-01-01",
-        updateDate: "2023-01-13",
-      },
-      {
-        id: 10,
-        name: "group 10",
-        createdDate: "2023-01-01",
-        updateDate: "2023-01-13",
-      },
-      {
-        id: 11,
-        name: "group 11",
-        createdDate: "2023-01-01",
-        updateDate: "2023-01-13",
-      },
-      {
-        id: 12,
-        name: "group 1",
-        createdDate: "2023-01-01",
-        updateDate: "2023-01-13",
-      },
-    ]);
-  }, []);
+    getAllGroup();
+  }, [page]);
 
   const deleteGroup = (e: any, groupId: number) => {
-    console.log(groupId);
+    ProductGroupAPI.deleteGroup(groupId)
+      .then((res) => {
+        toast.success(res.data.message);
+        getAllGroup();
+      })
+      .catch((err) => {
+        toast.error(err.response.data.msg);
+      });
   };
 
   const updateGroup = (e: any, groupName: string, groupId: number) => {
-    console.log(groupName);
-  };
-  const createGroup = (e: any) => {
-    ProductGroupAPI.create(newGroup)
+    ProductGroupAPI.update(groupName, groupId)
       .then((res) => {
-        console.log(res);
         toast.success(res.data.message);
+        getAllGroup();
       })
       .catch((err) => {
         console.log(err);
+        toast.error("Cannot Update. Try later");
+      });
+  };
+
+  const createGroup = (e: any) => {
+    ProductGroupAPI.create(newGroup)
+      .then((res) => {
+        toast.success(res.data.message);
+        getAllGroup();
+        setNewGroup("");
+      })
+      .catch((err) => {
         toast.error("Create new group failed. Try again later!");
       });
+  };
+
+  const changeStatus = (e: any, groupId: number, status: string) => {
+    if (status === "ENABLED") {
+      ProductGroupAPI.disableGroup(groupId)
+        .then((res) => {
+          toast.success(res.data.message);
+          getAllGroup();
+        })
+        .catch((err) => {
+          toast.error("Disable group failed. Try again later!");
+        });
+    } else {
+      ProductGroupAPI.enabledGroup(groupId)
+        .then((res) => {
+          toast.success(res.data.message);
+          getAllGroup();
+        })
+        .catch((err) => {
+          toast.error("Enable group failed. Try again later!");
+        });
+    }
+  };
+
+  const onSearchHandler = (e: any) => {
+    console.log(e.target.value);
+    const tempGroups = groups.filter((item) =>
+      item.name.includes(e.target.value)
+    );
+    setGroups(tempGroups);
   };
 
   const DeleteGroupModal = ({ groupName, groupId }: any) => {
@@ -113,7 +109,7 @@ const ProductGroup = () => {
           type="button"
           className="btn btn-danger"
           data-bs-toggle="modal"
-          data-bs-target="#staticBackdrop"
+          data-bs-target={`#deleteModal_${groupId}`}
         >
           Delete
         </button>
@@ -121,7 +117,7 @@ const ProductGroup = () => {
         {/* <!-- Modal --> */}
         <div
           className="modal fade"
-          id="staticBackdrop"
+          id={`deleteModal_${groupId}`}
           data-bs-keyboard="false"
           tabIndex={-1}
           aria-labelledby="staticBackdropLabel"
@@ -148,6 +144,7 @@ const ProductGroup = () => {
                 <button
                   type="button"
                   className="btn btn-danger"
+                  data-bs-dismiss="modal"
                   onClick={(e) => deleteGroup(e, groupId)}
                 >
                   Delete
@@ -160,8 +157,65 @@ const ProductGroup = () => {
     );
   };
 
+  const DisableEnableGroupModal = ({ groupName, groupId, status }: any) => {
+    return (
+      <div>
+        {/* <!-- Button trigger modal --> */}
+        <button
+          type="button"
+          className="btn btn-warning"
+          data-bs-toggle="modal"
+          data-bs-target={`#updateStatus${groupId}`}
+        >
+          {status === "DISABLED" ? "Enable" : "Disable"}
+        </button>
+
+        {/* <!-- Modal --> */}
+        <div
+          className="modal fade"
+          id={`updateStatus${groupId}`}
+          data-bs-keyboard="false"
+          tabIndex={-1}
+          aria-labelledby="staticBackdropLabel"
+          aria-hidden="true"
+        >
+          <div className="modal-dialog modal-dialog-centered">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h1 className="modal-title fs-5" id="staticBackdropLabel">
+                  {status === "DISABLED" ? "Enable" : "Disable"} Group
+                </h1>
+                <button
+                  type="button"
+                  className="btn-close"
+                  data-bs-dismiss="modal"
+                  aria-label="Close"
+                ></button>
+              </div>
+              <div className="modal-body">
+                Do you want to {status === "DISABLED" ? "Enable" : "Disable"}{" "}
+                group name:
+                <span className="text-danger"> {groupName}</span>
+              </div>
+              <div className="modal-footer">
+                <button
+                  type="button"
+                  className="btn btn-danger"
+                  data-bs-dismiss="modal"
+                  onClick={(e) => changeStatus(e, groupId, status)}
+                >
+                  Change
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  };
+
   const UpdateGroupModal = ({ groupName, groupId }: any) => {
-    const [newName, setNewName] = useState<string>("");
+    const [newName, setNewName] = useState<string>(groupName);
     return (
       <div>
         {/* <!-- Button trigger modal --> */}
@@ -169,7 +223,7 @@ const ProductGroup = () => {
           type="button"
           className="btn btn-primary"
           data-bs-toggle="modal"
-          data-bs-target="#updateModal"
+          data-bs-target={`#updateModal_${groupId}`}
         >
           Update
         </button>
@@ -177,7 +231,7 @@ const ProductGroup = () => {
         {/* <!-- Modal --> */}
         <div
           className="modal fade"
-          id="updateModal"
+          id={`updateModal_${groupId}`}
           data-bs-keyboard="false"
           tabIndex={-1}
           aria-labelledby="staticBackdropLabel"
@@ -215,6 +269,7 @@ const ProductGroup = () => {
                 <button
                   type="button"
                   className="btn btn-primary"
+                  data-bs-dismiss="modal"
                   onClick={(e) => updateGroup(e, newName, groupId)}
                 >
                   Update
@@ -251,6 +306,7 @@ const ProductGroup = () => {
                 type="text"
                 placeholder="Search in group name"
                 aria-label="Search"
+                onChange={(e) => onSearchHandler(e)}
               />
             </div>
 
@@ -291,13 +347,14 @@ const ProductGroup = () => {
                     className="form-control"
                     id="new-group-name"
                     value={newGroup}
-                    onChange={(e) => setNewGroup(e.target.value)}
+                    onFocus={(e) => setNewGroup(e.target.value)}
                   />
                 </div>
                 <div className="pt-3 text-center">
                   <button
                     type="button"
                     className="btn btn-primary"
+                    data-bs-dismiss="offcanvas"
                     onClick={createGroup}
                   >
                     Create
@@ -323,17 +380,88 @@ const ProductGroup = () => {
                 <tr key={item.id}>
                   <th scope="row">{item.id}</th>
                   <td>{item.name}</td>
-                  <td>{item.createdDate}</td>
-                  <td>{item.updateDate}</td>
-                  <td className="d-flex flex-row justify-content-center">
-                    <UpdateGroupModal groupName={item.name} groupId={item.id} />
-                    <p className="px-3"></p>
-                    <DeleteGroupModal groupName={item.name} groupId={item.id} />
+                  <td
+                    className={
+                      item.status === "REMOVED"
+                        ? "text-danger"
+                        : item.status === "DISABLED"
+                        ? "text-warning"
+                        : "text-success"
+                    }
+                  >
+                    {item.status}
                   </td>
+                  <td>{item.createdDate}</td>
+                  <td>{item.updatedDate}</td>
+                  {item.status !== "REMOVED" && (
+                    <td className="d-flex flex-row justify-content-center">
+                      <UpdateGroupModal
+                        groupName={item.name}
+                        groupId={item.id}
+                      />
+
+                      <p className="px-3"></p>
+                      <DisableEnableGroupModal
+                        groupName={item.name}
+                        groupId={item.id}
+                        status={item.status}
+                      />
+                      <p className="px-3"></p>
+                      <DeleteGroupModal
+                        groupName={item.name}
+                        groupId={item.id}
+                      />
+                    </td>
+                  )}
                 </tr>
               ))}
             </tbody>
           </table>
+        </div>
+
+        <div className="d-flex justify-content-between align-items-center">
+          <div className="summary">Total: {totalGroups} Groups</div>
+          <div className="pagination">
+            <nav aria-label="...">
+              <ul className="pagination">
+                <li className={page === 1 ? "page-item disabled" : "page-item"}>
+                  <a
+                    role="button"
+                    className="page-link"
+                    onClick={() => setPage(page - 1)}
+                  >
+                    Previous
+                  </a>
+                </li>
+                {[...Array(totalPage)].map((item, index) => (
+                  <li className="page-item" key={index}>
+                    <a
+                      role="button"
+                      onClick={() => setPage(index + 1)}
+                      className={
+                        page === index + 1 ? "page-link active" : "page-link"
+                      }
+                    >
+                      {index + 1}
+                    </a>
+                  </li>
+                ))}
+                <li
+                  className={
+                    totalPage === page ? "page-item disabled" : "page-item"
+                  }
+                >
+                  <a
+                    role="button"
+                    className="page-link"
+                    onClick={() => setPage(page + 1)}
+                  >
+                    Next
+                  </a>
+                </li>
+              </ul>
+            </nav>
+          </div>
         </div>
       </div>
     </div>
