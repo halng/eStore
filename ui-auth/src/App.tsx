e1b1b8af6b21cdc8220944e47df5a9b4fd921dfa
tanhaok <haonguyentan2001@gmail.com>
2023-08-28 18:36:26 +0700

    Update:

--- ui-auth/src/App.tsx
+++ ui-auth/src/App.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.css'
 import './app.css'
 
