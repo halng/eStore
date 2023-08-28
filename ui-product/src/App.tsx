e81b86ba6accf2877cc598ce0f52d9533471cfed
tanhaok <haonguyentan2001@gmail.com>
2023-08-28 18:57:35 +0700

    update

--- ui-product/src/App.tsx
+++ ui-product/src/App.tsx
@@ -1,28 +1,22 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { Route, Routes } from "react-router-dom";
-import { ToastContainer, toast } from "react-toastify";
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify'
 
-import "react-toastify/dist/ReactToastify.css";
-import "./index.css";
-import {
-  Product,
-  ProductAttribute,
-  ProductGroup,
-  ProductOption,
-} from "./components";
+import 'react-toastify/dist/ReactToastify.css'
+import './index.css'
+import { Product, ProductAttribute, ProductGroup, ProductOption } from './components'
 
 const App = () => (
-  <div className="">
-    <Routes>
-      <Route path="all" element={<Product />}></Route>
-      <Route path="option" element={<ProductOption />}></Route>
-      <Route path="attribute" element={<ProductAttribute />}></Route>
-      <Route path="group" element={<ProductGroup />}></Route>
-    </Routes>
-    <ToastContainer />
-  </div>
-);
+    <div className=''>
+        <Routes>
+            <Route path='all' element={<Product />}></Route>
+            <Route path='option' element={<ProductOption />}></Route>
+            <Route path='attribute' element={<ProductAttribute />}></Route>
+            <Route path='group' element={<ProductGroup />}></Route>
+        </Routes>
+        <ToastContainer />
+    </div>
+)
 
-export default App;
+export default App
 // ReactDOM.render(<App />, document.getElementById("app"));
--- ui-product/src/common/pagination/index.tsx
+++ ui-product/src/common/pagination/index.tsx
@@ -1,61 +1,45 @@
-import React from "react";
+import React from 'react'
 
 interface props {
-  total: number;
-  currentPage: number;
-  totalPage: number;
-  setPage: React.Dispatch<React.SetStateAction<number>>;
+    total: number
+    currentPage: number
+    totalPage: number
+    setPage: React.Dispatch<React.SetStateAction<number>>
 }
 
 const Pagination = ({ total, currentPage, setPage, totalPage }: props) => {
-  return (
-    <div className="d-flex justify-content-between align-items-center">
-      <div className="summary">Total: {total}</div>
-      <div className="pagination">
-        <nav aria-label="...">
-          <ul className="pagination">
-            <li
-              className={currentPage === 1 ? "page-item disabled" : "page-item"}
-            >
-              <a
-                role="button"
-                className="page-link"
-                onClick={() => setPage(currentPage - 1)}
-              >
-                Previous
-              </a>
-            </li>
-            {[...Array(totalPage)].map((item, index) => (
-              <li className="page-item" key={index}>
-                <a
-                  role="button"
-                  onClick={() => setPage(index + 1)}
-                  className={
-                    currentPage === index + 1 ? "page-link active" : "page-link"
-                  }
-                >
-                  {index + 1}
-                </a>
-              </li>
-            ))}
-            <li
-              className={
-                totalPage === currentPage ? "page-item disabled" : "page-item"
-              }
-            >
-              <a
-                role="button"
-                className="page-link"
-                onClick={() => setPage(currentPage + 1)}
-              >
-                Next
-              </a>
-            </li>
-          </ul>
-        </nav>
-      </div>
-    </div>
-  );
-};
+    return (
+        <div className='d-flex justify-content-between align-items-center'>
+            <div className='summary'>Total: {total}</div>
+            <div className='pagination'>
+                <nav aria-label='...'>
+                    <ul className='pagination'>
+                        <li className={currentPage === 1 ? 'page-item disabled' : 'page-item'}>
+                            <a role='button' className='page-link' onClick={() => setPage(currentPage - 1)}>
+                                Previous
+                            </a>
+                        </li>
+                        {[...Array(totalPage)].map((item, index) => (
+                            <li className='page-item' key={index}>
+                                <a
+                                    role='button'
+                                    onClick={() => setPage(index + 1)}
+                                    className={currentPage === index + 1 ? 'page-link active' : 'page-link'}
+                                >
+                                    {index + 1}
+                                </a>
+                            </li>
+                        ))}
+                        <li className={totalPage === currentPage ? 'page-item disabled' : 'page-item'}>
+                            <a role='button' className='page-link' onClick={() => setPage(currentPage + 1)}>
+                                Next
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    )
+}
 
-export default Pagination;
+export default Pagination
