58949ed7238b3153d911bec92068433a088c8fed
tanhaok <haonguyentan2001@gmail.com>
2023-09-22 17:41:15 +0700

    Test format with black

--- rating/app/main.py
+++ rating/app/main.py
@@ -5,9 +5,7 @@ from app.api.api import api_router
 from app.core.config import settings
 
 
-app = FastAPI(
-    title=settings.SERVER_NAME, openapi_url=f"{settings.API_V1_URL}/openapi.json"
-)
+app = FastAPI(title=settings.SERVER_NAME, openapi_url=f"{settings.API_V1_URL}/openapi.json")
 
 
 if settings.BACKEND_CORS_ORIGINS:
