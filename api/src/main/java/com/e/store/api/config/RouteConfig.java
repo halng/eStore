79389c418a4a82070ca4de6771aa935f59565f45
harold <haonguyentan2001@gmail.com>
2023-12-20 19:05:58 +0700

    dev: update docker file and format code

--- api/src/main/java/com/e/store/api/config/RouteConfig.java
+++ api/src/main/java/com/e/store/api/config/RouteConfig.java
@@ -1,7 +1,6 @@
 package com.e.store.api.config;
 
 import org.springframework.cloud.gateway.filter.factory.DedupeResponseHeaderGatewayFilterFactory;
-import org.springframework.cloud.gateway.filter.ratelimit.RedisRateLimiter;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
@@ -58,10 +57,5 @@ public class RouteConfig {
     ret.setStrategy(DedupeResponseHeaderGatewayFilterFactory.Strategy.RETAIN_FIRST);
     ret.setName("Access-Control-Allow-Origin");
     return ret;
-  }
-
-  @Bean
-  public RedisRateLimiter redisRateLimiter() {
-    return new RedisRateLimiter(1, 1, 1);
   }
 }
