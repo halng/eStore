90948580c7e0cfeca011b3250bbb4cfe275b68cf
Tan Hao Nguyen <101847895+tanhaok@users.noreply.github.com>
2023-12-25 18:51:42 +0700

    ES-144: add product api for library (#146)

--- ui-api/src/api/product/Product.ts
+++ ui-api/src/api/product/Product.ts
@@ -2,6 +2,10 @@ import api from '../helper/api'
 
 const ProductAPI = {
     create: (data: any) => api.post('/product', data),
+    getAll: (page: number) => api.get(`/product?page=${page}`),
+    getById: (id: string) => api.get(`/product/${id}`),
+    updateProduct: (id: string, data: any) => api.put(`/product/${id}`, data),
+    changeStatus: (id: string, action: string) => api.patch(`/product/${id}/${action}`),
 }
 
 export default ProductAPI
