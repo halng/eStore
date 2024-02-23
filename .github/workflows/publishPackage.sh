b4974b452adb0fd0470a86bf51bbf451f7c63811
harold <haonguyentan2001@gmail.com>
2024-02-23 20:11:45 +0700

    update[ui-api]: add bash script to publish

---
+++ .github/workflows/publishPackage.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e  # Exit on any error
+
+# Get the package name and version from package.json
+package_name=$(jq -r .name package.json)
+package_version=$(jq -r .version package.json)
+
+# Check if the version exists on the registry
+npm show $package_name@$package_version versions > /dev/null 2>&1
+if [[ $? -eq 0 ]]; then
+ # Version exists, comment on PR
+ gh pr comment --body "The version $package_version exists, Skip publish"
+else
+ # Version doesn't exist, publish and comment on PR
+ npm publish
+ gh pr comment --body "Publish new version $package_version success"
+fi
