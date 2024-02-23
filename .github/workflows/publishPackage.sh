99038f95cb09c1e3fb1220be684dd2ebfcb25e43
harold <haonguyentan2001@gmail.com>
2024-02-23 20:45:55 +0700

    update: add config for npm

--- .github/workflows/publishPackage.sh
+++ .github/workflows/publishPackage.sh
@@ -5,7 +5,7 @@
 cd ui-api
 
 npm config set registry https://registry.npmjs.org
-npm config set _authToken=$NODE_AUTH_TOKEN
+npm config set //registry.npmjs.org/:_authToken=$NODE_AUTH_TOKEN
 
 # Get the package name and version from package.json
 package_name=$(jq -r .name package.json)
